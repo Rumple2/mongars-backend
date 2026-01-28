9871e33dd2f981e48f0775e1ba22f0e007045e30
Galanga <117637819+Rumple2@users.noreply.github.com>
2026-01-28 12:13:30 +0000

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
+        'https://dashboard-weetoo.cekupay.com',
         env('FRONTEND_URL', 'http://localhost:5173'),
     ],
 
